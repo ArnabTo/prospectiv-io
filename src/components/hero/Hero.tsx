15e1f153c4c4c5a151f08959d7ec9effaf4ee86d
ArnabTo <arnabs929@gmail.com>
2024-10-17 22:17:37 +0600

    updated codebase

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,13 @@
+
+const Hero = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <div>
+                <h1>Get a reliable flow of qualified B2B leads</h1>
+                <p>Say goodbye to slow sales months. Sopro's multi-channel outreach service starts conversations with your ideal clients, so you can focus on selling more.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
