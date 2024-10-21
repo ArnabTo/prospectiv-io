e96dc101f9b052b301d839e70974ccc17e52ee56
ArnabTo <arnabs929@gmail.com>
2024-10-21 22:32:22 +0600

    updated

--- src/app/(app)/life-at-prospectiv/page.tsx
+++ src/app/(app)/life-at-prospectiv/page.tsx
@@ -1,9 +1,10 @@
-import {MemberCardMarquee } from '@/components/member-card/MemberCard';
+'use client';
+import { MemberCardMarquee } from '@/components/member-card/MemberCard';
 import Marquee from '@/components/ui/marquee';
 import HeaderImage from '@/public/assets/campany-culture.jpg'
 import Image from 'next/image';
 import { title } from 'process';
-
+import { motion } from 'framer-motion';
 const LifeAtProspectiv = () => {
 
     const membersDetails = [
@@ -46,11 +47,38 @@ const LifeAtProspectiv = () => {
                     <h1 className='text-6xl font-extrabold '>Life at <span className='text-secondary'>Prospectiv</span> </h1>
                     <p className='text-textColorTwo text-lg text-center font-bold mx-auto'>We're the people behind the prospects. For all the clever tech, what makes the Sopro service stand out is the people. As we like to tell our clients, we’re a team, not a tool.</p>
                 </div>
-
             </div>
 
-            <div className="max-w-7xl mx-auto py-24">
-                <MemberCardMarquee/>
+            <div className="max-w-7xl mx-auto py-24 space-y-24 ">
+                <div>
+                    <div className='w-1/2 mx-auto text-center'>
+                        <h3 className='text-4xl font-bold'>Meet Our People</h3>
+                        <p className='text-textColorTwo text-lg'>Together we have an amazing collection of knowledge and expertise.
+                            But it’s not all hard work: we strive to make Sopro a great place where we can grow, learn, achieve, and have fun.</p>
+                    </div>
+                    <div className='relative'>
+                        <MemberCardMarquee />
+
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.9 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 0.5, delay: 3.5 }}
+                            viewport={{ once: true }}
+                            className="blur-[100px] w-full max-w-[44%] min-h-[40%] absolute top-1/3 left-1/3 -z-10  transition-all">
+                            <div className="bg-[#6f86ff] absolute rounded-full opacity-50 w-[70%] min-w-[70%] min-h-[70%] max-h-[70%] top-0 right-0"></div>
+                            <div className="bg-[#fe316f] absolute rounded-full opacity-50 w-[70%] min-w-[70%] min-h-[70%] max-h-[70%] bottom-0 right-0"></div>
+                            <div className="bg-[#d220ff] absolute rounded-full opacity-50 w-[70%] min-w-[70%] min-h-[70%] max-h-[70%]"></div>
+                        </motion.div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className='w-1/2 mx-auto text-center'>
+                        <h3 className='text-4xl font-bold'>Our Teams</h3>
+                        <p className='text-textColorTwo text-lg'>These guys get their own page, but we love all our teams equally. If you want to know more about a specific team, whether you are thinking of applying to join one or just want to know more, take
+                            a look at their dedicated pages below.</p>
+                    </div>
+                </div>
             </div>
 
         </div>
--- src/components/member-card/MemberCard.tsx
+++ src/components/member-card/MemberCard.tsx
@@ -3,42 +3,42 @@ import Marquee from "../ui/marquee";
 import Image from "next/image";
 
 const reviews = [
-  {
-    name: "Jack",
-    username: "@jack",
-    body: "I've never seen anything like this before. It's amazing. I love it.",
-    img: "https://avatar.vercel.sh/jack",
-  },
-  {
-    name: "Jill",
-    username: "@jill",
-    body: "I don't know what to say. I'm speechless. This is amazing.",
-    img: "https://avatar.vercel.sh/jill",
-  },
-  {
-    name: "John",
-    username: "@john",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/john",
-  },
-  {
-    name: "Jane",
-    username: "@jane",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jane",
-  },
-  {
-    name: "Jenny",
-    username: "@jenny",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/jenny",
-  },
-  {
-    name: "James",
-    username: "@james",
-    body: "I'm at a loss for words. This is amazing. I love it.",
-    img: "https://avatar.vercel.sh/james",
-  },
+    {
+        name: "Jack",
+        username: "@jack",
+        body: "I've never seen anything like this before. It's amazing. I love it.",
+        img: "https://avatar.vercel.sh/jack",
+    },
+    {
+        name: "Jill",
+        username: "@jill",
+        body: "I don't know what to say. I'm speechless. This is amazing.",
+        img: "https://avatar.vercel.sh/jill",
+    },
+    {
+        name: "John",
+        username: "@john",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+        img: "https://avatar.vercel.sh/john",
+    },
+    {
+        name: "Jane",
+        username: "@jane",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+        img: "https://avatar.vercel.sh/jane",
+    },
+    {
+        name: "Jenny",
+        username: "@jenny",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+        img: "https://avatar.vercel.sh/jenny",
+    },
+    {
+        name: "James",
+        username: "@james",
+        body: "I'm at a loss for words. This is amazing. I love it.",
+        img: "https://avatar.vercel.sh/james",
+    },
 ];
 
 
@@ -76,37 +76,37 @@ const firstRow = membersDetails.slice(0, reviews.length / 2);
 
 
 
-const MemberCard =({name, title, avatar, says}: {name: string, title: string, avatar: string, says: string}) => {
+const MemberCard = ({ name, title, avatar, says }: { name: string, title: string, avatar: string, says: string }) => {
     return (
-        <figure
-      className={cn(
-        "relative w-64 cursor-pointer overflow-hidden rounded-xl border p-4",
-      )}
-    >
-      <div className="flex flex-row items-center gap-2">
-        <Image src={avatar} alt="" width={32} height={32}/>
-        <div className="flex flex-col">
-          <figcaption className="text-sm font-medium dark:text-white">
-            {name}
-          </figcaption>
-          <p className="text-xs font-medium dark:text-white/40">{title}</p>
+        <div
+            className='relative w-[40rem] cursor-pointer overflow-hidden rounded-xl border bg-card border-borderColor p-4'
+        >
+            <div className="flex flex-row items-center gap-5">
+
+                <Image className="rounded-full w-32 h-32" src={avatar} alt="member-image" width={100} height={100} />
+                <div className="flex flex-col">
+                    <figcaption className="text-lg font-medium dark:text-white">
+                        {name}
+                    </figcaption>
+                    <p className="text-lg font-medium dark:text-white/40">{title}</p>
+                    <blockquote className="mt-2 text-2xl text-textColorTwo">{says}</blockquote>
+                </div>
+            </div>
+            {/* <div className="bg-gradient-to-r from-[#6f86ff] via-[#fe316f] to-[#d220ff] opacity-30 blur-3xl absolute inset-0 -z-10"></div> */}
         </div>
-      </div>
-      <blockquote className="mt-2 text-sm">{says}</blockquote>
-    </figure>
     )
 }
 
 export function MemberCardMarquee() {
-  return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background">
-      <Marquee pauseOnHover className="[--duration:20s]">
-        {membersDetails.map((member) => (
-          <MemberCard key={member.name} {...member} />
-        ))}
-      </Marquee>
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-background "></div>
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-background  "></div>
-    </div>
-  );
+    return (
+        <div className="relative flex h-[250px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-transparent z-50">
+            <Marquee pauseOnHover className="[--duration:20s]">
+                {membersDetails.map((member) => (
+                    <MemberCard key={member.name} {...member} />
+                ))}
+            </Marquee>
+            <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r"></div>
+            <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l"></div>
+        </div>
+    );
 }
