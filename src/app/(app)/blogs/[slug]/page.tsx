91f8f8e888bce73f19c4213a3a5cbaf16b0f4ba9
ArnabTo <arnabs929@gmail.com>
2024-10-29 00:53:14 +0600

    updated

--- src/app/(app)/blogs/[slug]/page.tsx
+++ src/app/(app)/blogs/[slug]/page.tsx
@@ -19,6 +19,7 @@ import InstagramLogo from '@/public/assets//icons8-instagram-480.png'
 import FacebookLogo from '@/public/assets/icons8-facebook-480.png'
 import { slugify } from "@/utils/helper";
 import ScrollProgressBar from "@/components/PageScrollProgressBar/ProgressBar";
+import { Separator } from "@/components/ui/separator";
 
 // interface Params {
 //     params: {
@@ -57,6 +58,7 @@ interface Blog {
 const BlogDetails = () => {
 
     const params = useParams();
+
     const [blog, setBlog] = useState<Blog>()
     const [isLoading, setIsLoading] = useState(false)
 
@@ -80,84 +82,95 @@ const BlogDetails = () => {
     const readingTime = blog ? calculateReadingTime(blog.body) : "";
 
     return (
-        <div className="space-y-20">
-            <ScrollProgressBar/>
-            {
-                isLoading ? (
-                    <div className="flex justify-center items-center h-screen">
-                        <Loader2 size={60} className="w-10 h-10 mx-auto animate-spin" />
-                    </div>
-                )
-                    :
-                    (
-                        <div className="max-w-screen-2xl mx-auto">
-                            {
-                                blog && (
-                                    <div className="max-w-7xl ml-auto md:grid md:grid-cols-4 gap-5 px-5 lg:px-10">
-                                        <div className="col-span-3">
-                                            <article className="space-y-10">
-                                                <section>
-                                                    <div>
-                                                        <Image className="w-full h-3/4 object-contain rounded-2xl" src={blog?.mainImage?.asset?.url} width={500} height={500} alt="blog_thumbnail" />
-                                                        <div className="flex items-center justify-start gap-5 mt-3">
-                                                            {
-                                                                blog.categories == null ? <>"Unknown"</>
-                                                                    :
-                                                                    (<p className="flex items-center gap-2 text-md text-textColorTwo">
-                                                                        <Tag size={20} />
-                                                                        {blog.categories.map((category, index) => (<span key={index}>{category.title}</span>))}</p>)
-                                                            }
+        <div>
+            <ScrollProgressBar />
+            <div>
+                <Separator orientation="horizontal" />
+            </div>
+            <div className="space-y-20">
 
-                                                            <p className="flex items-center gap-2 text-md text-textColorTwo"><Calendar size={20} /> {new Date(blog?._createdAt).toDateString()}</p>
-                                                            <p className="flex items-center gap-2 text-md text-textColorTwo"><Calendar size={20} /> {readingTime}</p>
-                                                        </div>
-                                                    </div>
-                                                    <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold mt-8">{blog.title}</h1>
-                                                </section>
-                                                <div>
-                                                    <PortableText value={blog.body} components={RichTextComponent} />
-                                                </div>
-                                            </article>
-
-                                        </div>
-                                        <div className="hidden md:block col-span-1 sticky top-16 space-y-5"
-                                            style={{
-                                                height: `calc(100vh - 95px)`
-                                            }}
-                                        >
-                                            <div className="border border-borderColor rounded-2xl p-5 space-y-4 hover:shadow-2xl group cursor-pointer">
-                                                <div className="flex items-center gap-3">
-                                                    <Image className="w-1/4 rounded-full group-hover:scale-105 transition-all duration-300 ease-in-out"
-                                                        src={blog?.author?.image?.asset?.url} width={500} height={500}
-                                                        alt="author_image" />
-                                                    <span>
-                                                        <p className="text-foreground text-lg font-bold group-hover:text-secondary">{blog?.author?.name}</p>
-                                                        <PortableText value={blog.author.bio} components={RichTextComponent} />
-                                                    </span>
-                                                </div>
-                                                <div className="flex flex-row gap-3 justify-center items-center">
-                                                    <div className='bg-smallCard rounded-lg hover:scale-105 transition-all duration-300 ease-in-out hover:bg-secondary'>
-                                                        <Link href='#'><Image className='max-w-10 p-2' src={LinkedInLogo} width={200} height={200} alt="LinkedIn" /></Link>
-                                                    </div>
-                                                    <div className='bg-smallCard rounded-lg hover:scale-105 transition-all duration-300 ease-in-out hover:bg-secondary'>
-                                                        <Link href='#'><Image className='max-w-10 p-2' src={InstagramLogo} width={200} height={200} alt="LinkedIn" /></Link>
-                                                    </div>
-                                                    <div className='bg-smallCard rounded-lg hover:scale-105 transition-all duration-300 ease-in-out hover:bg-secondary'>
-                                                        <Link href='#'><Image className='max-w-10 p-2' src={FacebookLogo} width={200} height={200} alt="LinkedIn" /></Link>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <Toc headings={blog.headings} />
-                                        </div>
-                                    </div>
-                                )
-                            }
+                {
+                    isLoading ? (
+                        <div className="flex justify-center items-center h-screen">
+                            <Loader2 size={60} className="w-10 h-10 mx-auto animate-spin" />
                         </div>
                     )
-            }
-            <div>
-                <BlogSection />
+                        :
+                        (
+                            <div className="max-w-screen-2xl mx-auto">
+                                {
+                                    blog && (
+                                        <div className="max-w-7xl ml-auto md:grid md:grid-cols-4 gap-5 px-5 lg:px-10">
+                                            <div className="col-span-3 py-16">
+                                                <div>
+                                                    <article className="space-y-10">
+                                                        <section>
+                                                            <div>
+                                                                <Image className="w-full h-3/4 object-contain rounded-2xl" src={blog?.mainImage?.asset?.url} width={500} height={500} alt="blog_thumbnail" />
+                                                                <div className="flex items-center justify-start gap-5 mt-3">
+                                                                    {
+                                                                        blog.categories == null ? <>"Unknown"</>
+                                                                            :
+                                                                            (<p className="flex items-center gap-2 text-md text-textColorTwo">
+                                                                                <Tag size={20} />
+                                                                                {blog.categories.map((category, index) => (<span key={index}>{category.title}</span>))}</p>)
+                                                                    }
+
+                                                                    <p className="flex items-center gap-2 text-md text-textColorTwo"><Calendar size={20} /> {new Date(blog?._createdAt).toDateString()}</p>
+                                                                    <p className="flex items-center gap-2 text-md text-textColorTwo"><Calendar size={20} /> {readingTime}</p>
+                                                                </div>
+                                                            </div>
+                                                            <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold mt-8">{blog.title}</h1>
+                                                        </section>
+                                                        <section>
+                                                            <PortableText value={blog.body} components={RichTextComponent} />
+                                                        </section>
+                                                    </article>
+
+                                                </div>
+                                            </div>
+                                            <div className="hidden md:block col-span-1 sticky top-16 border-l border-borderColor px-3"
+                                                style={{
+                                                    height: `calc(100vh - 95px)`
+                                                }}
+                                            >
+                                                <div className="py-16 space-y-5">
+                                                    <div className="border border-borderColor rounded-2xl p-5 space-y-4 hover:shadow-2xl group cursor-pointer">
+                                                        <div className="flex items-center gap-3">
+                                                            <Image className="w-1/4 rounded-full group-hover:scale-105 transition-all duration-300 ease-in-out"
+                                                                src={blog?.author?.image?.asset?.url} width={500} height={500}
+                                                                alt="author_image" />
+                                                            <span>
+                                                                <p className="text-lg font-bold text-secondary">{blog?.author?.name}</p>
+                                                                <PortableText value={blog.author.bio} components={RichTextComponent} />
+                                                            </span>
+                                                        </div>
+                                                        <div className="flex flex-row gap-3 justify-center items-center">
+                                                            <div className='bg-smallCard rounded-lg hover:scale-105 transition-all duration-300 ease-in-out hover:bg-secondary'>
+                                                                <Link href='#'><Image className='max-w-10 p-2' src={LinkedInLogo} width={200} height={200} alt="LinkedIn" /></Link>
+                                                            </div>
+                                                            <div className='bg-smallCard rounded-lg hover:scale-105 transition-all duration-300 ease-in-out hover:bg-secondary'>
+                                                                <Link href='#'><Image className='max-w-10 p-2' src={InstagramLogo} width={200} height={200} alt="LinkedIn" /></Link>
+                                                            </div>
+                                                            <div className='bg-smallCard rounded-lg hover:scale-105 transition-all duration-300 ease-in-out hover:bg-secondary'>
+                                                                <Link href='#'><Image className='max-w-10 p-2' src={FacebookLogo} width={200} height={200} alt="LinkedIn" /></Link>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    <Toc headings={blog.headings} />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        )
+                }
+
+                <div>
+                    <BlogSection />
+                </div>
             </div>
         </div>
     );
