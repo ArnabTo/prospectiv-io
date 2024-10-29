7fb048b5e5170138b36c150ea64748fd28e06770
ArnabTo <arnabs929@gmail.com>
2024-10-29 16:23:17 +0600

    updated

--- src/components/Blogs/BlogSection.tsx
+++ src/components/Blogs/BlogSection.tsx
@@ -1,25 +1,13 @@
 import { fetchBlogs } from "@/utils/fetchBlogs";
 import { useCallback, useEffect, useState } from "react";
-import { Card, CardContent, CardHeader } from "../ui/card";
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import Image from "next/image";
 import Link from "next/link";
 import axios from 'axios';
 import { motion } from 'framer-motion';
+import { Blog } from "@/types/types";
 
-interface Blog {
-    _id: string;
-    title: string;
-    slug: {
-        current: string;
-    };
-    mainImage: {
-        asset: {
-            url: string
-        }
-    };
-    _createdAt: string
-}
-const BlogSection: React.FC = () => {
+const BlogSection = () => {
 
     const [blogs, setBlogs] = useState<Blog[]>([])
 
@@ -41,27 +29,31 @@ const BlogSection: React.FC = () => {
             <div className="max-w-7xl mx-auto px-5 lg:py-24">
                 <h1 className="text-3xl md:text-4xl lg:text-4xl font-bold text-center">Discover how Prospectiv helps you sell more</h1>
                 <motion.div
-                initial={{ opacity: 0, scale: 0.9 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.5 }}
-                viewport={{ once: true }}
-                className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10 mt-10">
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.5 }}
+                    viewport={{ once: true }}
+                    className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-10 mt-10">
                     {
                         blogs.length > 0 && blogs.map((blog) => {
                             return (
                                 <Link href={`/blogs/${blog.slug.current}`} key={blog._id}>
-                                    <Card className="border border-borderColor flex flex-col h-full rounded-2xl">
+                                    <Card className="border border-borderColor flex flex-col h-full rounded-2xl group">
                                         <CardHeader className="flex-grow">
                                             <h3 className="text-xl font-bold">{blog?.title}</h3>
-                                            <Image
-                                                className="rounded-xl"
-                                                src={blog.mainImage.asset.url}
-                                                width={500} height={500}
-                                                alt="blog" />
                                         </CardHeader>
-                                        <CardContent className="mt-auto">
-                                            <p className="text-md lg:text-lg text-textColorTwo">published at: {new Date(blog?._createdAt).toDateString()}</p>
+                                        <CardContent>
+                                            <div className="overflow-hidden">
+                                                <Image
+                                                    className="rounded-xl group-hover:scale-105 transition-all duration-300 ease-in-out"
+                                                    src={blog.mainImage.asset.url}
+                                                    width={500} height={500}
+                                                    alt="blog" />
+                                            </div>
                                         </CardContent>
+                                        <CardFooter>
+                                            <p className="text-md lg:text-lg text-textColorTwo">published at: {new Date(blog?._createdAt).toDateString()}</p>
+                                        </CardFooter>
                                     </Card>
                                 </Link>
                             )
