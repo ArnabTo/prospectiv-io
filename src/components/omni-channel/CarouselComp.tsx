608684259caf6dcdcace64e685e44ef34d3612ee
ArnabTo <arnabs929@gmail.com>
2024-12-05 17:49:07 +0600

    updated

--- src/components/omni-channel/CarouselComp.tsx
+++ src/components/omni-channel/CarouselComp.tsx
@@ -7,57 +7,71 @@ import CarouselCard from './CarsouselCard';
 
 const CarouselComp = () => {
 
-    
-const projects = [
-    {
-        title: "Matthias Leidinger",
-        description: "Originally hailing from Austria, Berlin-based photographer Matthias Leindinger is a young creative brimming with talent and ideas.",
-        src: "https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
-        color: "#BBACAF"
-    },
-    {
-        title: "Clément Chapillon",
-        description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes”—so French photographer Clément Chapillon describes his latest highly captivating project Les rochers fauves (French for ‘The tawny rocks’).",
-        src: "https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
-        color: "#977F6D"
-    },
-    {
-        title: "Zissou",
-        description: "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
-        src: "https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
-        color: "#C2491D"
-    },
- 
-]
+
+    const projects = [
+        {
+            title: "Cold Email",
+            description: `Cold email is a cost-effective method that allows for highly personalised and direct
+outreach to decision-makers, thriving in niche markets like HR tech, cybersecurity, and
+boutique consulting, where scalability and customisation are key advantages.`,
+            src: "https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
+            color: "#BBACAF"
+        },
+        {
+            title: "LinkedIn Ads",
+            description: `LinkedIn Ads excels in precisely targeting specific professional roles and reaching a
+professional audience, making it perfect for industries like B2B SaaS, professional services,
+and consulting, where tailored ad formats and content promotion drive engagement.`,
+            src: "https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
+            color: "#977F6D"
+        },
+        {
+            title: "Google Ads.",
+            description: `Google Ads targets high-intent users actively searching for solutions, making it highly
+effective for industries like software, IT services, and industrial equipment, with the ability
+to generate fast results and scale campaigns efficiently`,
+            src: "https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
+            color: "#C2491D"
+        },
+
+    ]
 
     const container = useRef(null);
     const { scrollYProgress } = useScroll({
-      target: container,
-      offset: ['start start', 'end end']
+        target: container,
+        offset: ['start start', 'end end']
     })
-  
-    useEffect( () => {
-      const lenis = new Lenis()
-  
-      function raf(time:any) {
-        lenis.raf(time)
+
+    useEffect(() => {
+        const lenis = new Lenis()
+
+        function raf(time: any) {
+            lenis.raf(time)
+            requestAnimationFrame(raf)
+        }
+
         requestAnimationFrame(raf)
-      }
-  
-      requestAnimationFrame(raf)
     })
 
     return (
         <div ref={container} className='relative mt-20'>
-            {
-                projects.map((project, i) => {
-                    const targetScale = 1 - ((projects.length - i) * 0.05);
-                    return <CarouselCard key={`p_${i}`} i={i} {...project} progress={scrollYProgress} range={[i * .25, 1]} targetScale={targetScale} />
-                })
-            }
+            <div>
+                {
+                    projects.map((project, i) => {
+                        const targetScale = 1 - ((projects.length - i) * 0.05);
+                        return <CarouselCard key={`p_${i}`} i={i} {...project} progress={scrollYProgress} range={[i * .25, 1]} targetScale={targetScale} />
+                    })
+                }
+            </div>
+            <div
+                className="opacity-[0.5] blur-[100px] w-full max-w-[44%] min-h-[74%] absolute bottom-12 right-10 lg:bottom-12 lg:-right-10 -z-10 transition-all">
+                <div className="bg-gradientColorFour absolute rounded-full opacity-50 w-[70%] min-w-[70%] min-h-[70%] max-h-[70%] top-0 right-0"></div>
+                <div className="bg-gradientColorFive absolute rounded-full opacity-50 w-[70%] min-w-[70%] min-h-[70%] max-h-[70%] bottom-0 right-0"></div>
+                <div className="bg-gradientColorFour absolute rounded-full opacity-50 w-[70%] min-w-[70%] min-h-[70%] max-h-[70%]"></div>
+            </div>
         </div>
 
     );
--- src/components/omni-channel/CarsouselCard.tsx
+++ src/components/omni-channel/CarsouselCard.tsx
@@ -3,9 +3,10 @@ import Image from 'next/image';
 import { useTransform, motion, useScroll } from 'framer-motion';
 import { useRef } from 'react';
 import './carouselStyle.css';
+import Link from 'next/link';
 
 
-const CarouselCard = ({i, title, description, src, url, color, progress, range, targetScale}: any) => {
+const CarouselCard = ({ i, title, description, src, url, color, progress, range, targetScale }: any) => {
 
   const container = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -15,47 +16,45 @@ const CarouselCard = ({i, title, description, src, url, color, progress, range, 
 
   const imageScale = useTransform(scrollYProgress, [0, 1], [2, 1])
   const scale = useTransform(progress, range, [1, targetScale]);
- 
-  return (
-    <div ref={container} className="cardContainer">
-    <motion.div
-      style={{
-        scale,
-        top: `calc(-5vh + ${i * 25}px)`,
-      }}
-      className="card border border-borderColor"
-    >
-      <div className="body">
-        <div className="description bg-buttonHoverColor rounded-l-3xl">
-          <p>{description}</p>
-          <span>
-            <a href={url} target="_blank" rel="noopener noreferrer">
-              See more
-            </a>
-            <svg width="22" height="12" viewBox="0 0 22 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M21.5303 6.53033C21.8232 6.23744 21.8232 5.76256 21.5303 5.46967L16.7574 0.696699C16.4645 0.403806 15.9896 0.403806 15.6967 0.696699C15.4038 0.989592 15.4038 1.46447 15.6967 1.75736L19.9393 6L15.6967 10.2426C15.4038 10.5355 15.4038 11.0104 15.6967 11.3033C15.9896 11.5962 16.4645 11.5962 16.7574 11.3033L21.5303 6.53033ZM0 6.75L21 6.75V5.25L0 5.25L0 6.75Z"
-                fill="black"
-              />
-            </svg>
-          </span>
-        </div>
 
-        <div className="imageContainer bg-card">
-          {/* <motion.div className="inner" style={{ scale: imageScale }}>
+  return (
+    <div ref={container} className="cardContainer relative">
+      <motion.div
+        style={{
+          scale,
+          top: `calc(-5vh + ${i * 25}px)`,
+        }}
+        className="card border border-borderColor"
+      >
+        <div className="flex h-full">
+          <div className="bg-buttonHoverColor w-1/2 relative p-12 rounded-l-3xl space-y-3">
+            <h2 className='text-5xl pt-8 pb-1 font-bold text-start text-secondary'>{title}.</h2>
+            <p className=' text-textColorTwo text-lg'>{description}</p>
+
+            <div
+              className="w-60 flex rounded-full bg-gradient-to-tr from-gradientColorOne via-[#b372ce] to-[#ff7586] p-[2px] shadow-lg duration-300 transform group-hover:scale-105">
+              <Link href="/demo" className="flex-1 font-bold text-lg bg-black px-10 xl:px-16 py-3 rounded-full hover:scale-95 transition-all duration-300">
+                Learn more.
+              </Link>
+            </div>
+          </div>
+
+          <div className="imageContainer bg-card p-12">
+            {/* <motion.div className="inner" style={{ scale: imageScale }}>
             <Image  width={500} height={500} src='https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt="image" />
           </motion.div> */}
-          <motion.div className="inner" style={{ scale: imageScale }}>
-          <Image 
-          width={500}
-          height={500}
-          alt=''
-          src='https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'/>
-          </motion.div>
+            <motion.div className="w-full h-full" style={{ scale: imageScale }}>
+              <Image
+                className='rounded-3xl h-full'
+                width={1000}
+                height={1000}
+                alt=''
+                src='https://images.pexels.com/photos/18876270/pexels-photo-18876270/free-photo-of-red-cabin-on-a-cliff.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
+            </motion.div>
+          </div>
         </div>
-      </div>
-    </motion.div>
-  </div>
+      </motion.div>
+    </div>
   )
 }
 
