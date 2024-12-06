6d6a58b8358b0e099c2e150d4db04548f7fc088c
ArnabTo <arnabs929@gmail.com>
2024-12-06 21:35:47 +0600

    updated

--- src/components/walkthrough/Walkthrough.tsx
+++ src/components/walkthrough/Walkthrough.tsx
@@ -6,7 +6,7 @@ const Walkthrough = () => {
         <div className='max-w-7xl mx-auto p-5'>
             <h2 className='text-3xl md:text-4xl lg:text-5xl text-center lg:text-left font-semibold mb-16'>Paid Advertising Walkthrough</h2>
             <div className='w-full space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent'>
-                <div className='relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse gap-2 group'>
+                <div className='relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse gap-2 lg:gap-0 group'>
                     {/* Icon */}
                     <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
                         <ArrowDown />
@@ -33,7 +33,7 @@ const Walkthrough = () => {
                     </div>
                 </div>
 
-                <div className='relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse gap-2 group'>
+                <div className='relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse gap-2 lg:gap-0 group'>
                     {/* Icon */}
                     <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
                         <ArrowDown />
@@ -63,7 +63,7 @@ const Walkthrough = () => {
                     </div>
                 </div>
 
-                <div className='relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse gap-2 group'>
+                <div className='relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse gap-2 lg:gap-0 group'>
                     {/* Icon */}
                     <div className="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2">
                         <ArrowDown />
