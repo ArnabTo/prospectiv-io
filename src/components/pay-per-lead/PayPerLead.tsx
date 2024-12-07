4155abdfe12d329097e05eb777e63006e64d5f56
ArnabTo <arnabs929@gmail.com>
2024-12-07 10:16:13 +0600

    updated payperlead

--- src/components/pay-per-lead/PayPerLead.tsx
+++ src/components/pay-per-lead/PayPerLead.tsx
@@ -1,4 +1,4 @@
-import { ArrowRightCircle } from 'lucide-react';
+import { ArrowRight, ArrowRightCircle } from 'lucide-react';
 import { useEffect } from 'react';
 import { useRef } from 'react';
 import gsap from 'gsap';
@@ -76,8 +76,8 @@ const PayPerLead = () => {
             <div className="flex flex-col lg:flex-row relative">
                 <div className="space-y-5">
                     <div
-                    ref={animateRef}
-                    className="space-y-2 text-center">
+                        ref={animateRef}
+                        className="space-y-2 text-center">
                         <h2 className="text-3xl md:text-4xl lg:text-4xl font-bold">Pay per Lead</h2>
                         <p className="text-md lg:text-lg text-textColorTwo lg:max-w-[80%] mx-auto">Committed to our confidence in our lead generation system, Prospectiv’s lead generation
                             services come without a fixed-term contract. You only pay for as few or as many leads as
@@ -86,31 +86,31 @@ const PayPerLead = () => {
                     </div>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center">
                         <div
-                        ref={cardOne}
-                        className="bg-card border-l-2 border-secondary p-5 flex items-center justify-between group hover:bg-[#fe316f0a] tramsition duration-300 cursor-pointer rounded-r-xl">
-                            <div>
+                            ref={cardOne}
+                            className="bg-card border-l-2 border-secondary flex items-center justify-between group tramsition duration-300 rounded-r-xl">
+                            <div className=' p-5 min-w-[80%]'>
                                 <h3 className="text-2xl font-bold">Choose How Many</h3>
                                 <p className="text-lg text-textColorTwo">Start From 10 Leads.</p>
                             </div>
-                            <ArrowRightCircle size={40} className='group-hover:text-secondary tramsition duration-300 ' />
+                            <ArrowRight size={40} className='group-hover:text-secondary tramsition duration-300 ' />
                         </div>
                         <div
-                        ref={cardTwo}
-                        className="bg-card border-l-2 border-secondary p-5 flex items-center justify-between group hover:bg-[#fe316f0a] tramsition duration-300 cursor-pointer rounded-r-xl">
-                            <div>
+                            ref={cardTwo}
+                            className="bg-card border-l-2 border-secondary flex items-center justify-between group tramsition duration-300 rounded-r-xl">
+                          <div className=' p-5 min-w-[80%]'>
                                 <h3 className="text-2xl font-bold">Campaign Launch</h3>
                                 <p className="text-lg text-textColorTwo">Launch In 2 Weeks.</p>
                             </div>
-                            <ArrowRightCircle size={40} className='group-hover:text-secondary tramsition duration-300 ' />
+                            <ArrowRight size={40} className='group-hover:text-secondary tramsition duration-300 ' />
                         </div>
                         <div
-                        ref={cardThree}
-                        className="bg-card border-l-2 border-secondary p-5 flex items-center justify-between group hover:bg-[#fe316f0a] tramsition duration-300 cursor-pointer rounded-r-xl">
-                            <div>
+                            ref={cardThree}
+                            className="bg-card border-l-2 border-secondary flex items-center justify-between group tramsition duration-300 rounded-r-xl">
+                           <div className=' p-5 min-w-[80%]'>
                                 <h3 className="text-2xl font-bold">Leads Delivered</h3>
                                 <p className="text-lg text-textColorTwo">And Booked Into Meetings.</p>
                             </div>
-                            <ArrowRightCircle size={40} className='group-hover:text-secondary tramsition duration-300 ' />
+                            <ArrowRight size={40} className='group-hover:text-secondary tramsition duration-300 ' />
                         </div>
                     </div>
                 </div>
