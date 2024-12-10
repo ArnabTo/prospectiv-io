c4f2a8aa42a8c8703f9f4f8825c5907bfa42c436
ArnabTo <arnabs929@gmail.com>
2024-12-10 23:18:26 +0600

    updated

--- src/app/api/getallblogs/route.ts
+++ src/app/api/getallblogs/route.ts
@@ -4,7 +4,13 @@ import { client } from "@/sanity/lib/client";
 export async function GET(reqest: Request) {
     try {
         const response = await client.fetch(ALL_POST_QUERY);
-        return new Response(JSON.stringify(response), { status: 200 });
+        return new Response(JSON.stringify(response), {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+                "Cache-Control": "no-store", // Prevent caching to always fetch fresh data
+            },
+        });
     } catch (error) {
         console.error(error);
         return new Response(JSON.stringify({ message: "Failed to fetch blogs" }), { status: 500 });
