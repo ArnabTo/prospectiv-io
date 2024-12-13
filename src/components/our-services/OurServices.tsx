ce56357832313ff1765f2238594f62cc2f133f9b
ArnabTo <arnabs929@gmail.com>
2024-12-13 16:19:44 +0600

    updated css

--- src/components/our-services/OurServices.tsx
+++ src/components/our-services/OurServices.tsx
@@ -5,7 +5,7 @@ const OurServices = () => {
         <div>
             <div className="max-w-7xl mx-auto pt-16">
                 <h1 className="text-3xl md:text-5xl font-bold text-center">Our Services</h1>
-                <p className="text-md md:text-lg text-textColorTwo text-center">High-impact marketing services to grow your startup</p>
+                <p className="text-md md:text-xl text-textColorTwo text-center">High-impact marketing services to grow your startup</p>
                 <CarouselComp/>
             </div>
         </div>
